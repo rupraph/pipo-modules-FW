23beaaced6c9ab770280501887f7b4adf84cf0fb
Rup <rupertrenevier@hotmail.com>
2024-02-25 15:21:19 +0100

    added wifimanager

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <fs_tools.h>
+#include <WiFiManager.h> 
 
 #define FORMAT_LITTLEFS_IF_FAILED true
 
@@ -11,6 +12,31 @@ void setup(){
     }
     Serial.println("LittleFS Mount Success");
 
+
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    // wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
     // init wifi
   // init webserver
   // init BLE
