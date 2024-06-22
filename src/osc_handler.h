aaa76000880f7b4ce413fb1b7d01c4d43ac2903a
Leo Nicolle <leo@linkurio.us>
2024-06-22 20:17:12 +0200

    make it build

--- src/osc_handler.h
+++ src/osc_handler.h
@@ -1,19 +1,20 @@
 #ifndef OSC_HANDLER_H
 #define OSC_HANDLER_H
 #include <OSCMessage.h>
-#include <WiFiUDP.h>
+#include <WiFiUdp.h>
 
-class OSC_handler {
-    public:
-        WiFiUDP Udp;
-        IPAddress dest_ip;
-        int out_port;
-        bool isStarted = false;
-        void start();
-        void stop();
-        void sendOscMessage(int value);
-        void setDestIp(IPAddress ip);
-        void setoutPort(int port);
+class OSC_handler
+{
+public:
+    WiFiUDP Udp;
+    IPAddress dest_ip;
+    int out_port;
+    bool isStarted = false;
+    void start();
+    void stop();
+    void sendOscMessage(int value);
+    void setDestIp(IPAddress ip);
+    void setoutPort(int port);
 };
 
-#endif //OSC_HANDLER_H
+#endif // OSC_HANDLER_H
