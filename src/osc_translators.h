55243eaf6441417a0fa415847a866ecf34417862
Rup <rupertrenevier@hotmail.com>
2024-08-26 17:57:51 +0200

    minor debug

--- src/osc_translators.h
+++ src/osc_translators.h
@@ -11,7 +11,7 @@ using namespace std;
 class OscTranslator
 {
     public:
-        OscTranslator();// = default;
+        //OscTranslator();// = default;
         // OscTranslator(float input_min, float input_max): input_min(input_min), input_max(input_max) {}
 
         bool enabled=false;
@@ -21,7 +21,7 @@ class OscTranslator
         float output_min=0;
 
         float get_value(float value, float min_input, float max_input);
-        int get_current_bool(float value);
+        int get_current_bool(float value, float min_input, float max_input);
 
         void to_json(nlohmann::json& j, const OscTranslator& t);
         void from_json(const nlohmann::json& j, OscTranslator& t);
