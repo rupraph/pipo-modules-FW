634edc5b47116915c5e8ab54dae1a8e335ea89d9
Rup <rupertrenevier@hotmail.com>
2025-02-14 20:23:22 +0100

    cleanup analog UI form

--- web-config/src/lib/configs/analog-out.svelte
+++ web-config/src/lib/configs/analog-out.svelte
@@ -3,31 +3,26 @@
   import Checkbox from "../form/Checkbox.svelte";
   import Tooltip from "../tooltip/Tooltip.svelte";
   export let outconfig;
+  console.log(outconfig);
 </script>
 
 <div class="analog-out">
-  <div class="row">
-    <div class="header">Pin</div>
-    <div class="header">Direction</div>
-    <div class="header">Out Mode</div>
-  </div>
-  {#each Object.entries(outconfig.analogout) as [key, value]}
-    <div class="column">
-      <div class="cell">{key}</div>
-      <div class="cell">
-        <label class="checkbox">
-          <input type="checkbox" bind:checked={value.pindir} />
-          <span class="checkmark"></span>
-        </label>
-      </div>
-
-      <div class="cell">
-        <label class="checkbox">
-          <input type="checkbox" bind:checked={value.outmode} />
-          <span class="checkmark"></span>
-        </label>
-      </div>
-    </div>
+  <span class="row-header" style="grid-row:1">Axis</span>
+  <span class="row-header" style="grid-row:2">Dir</span>
+  <span class="row-header" style="grid-row:3">Out Mode</span>
+  {#each Object.entries(outconfig.analogout) as [key, value], i}
+    <span class="column" style="grid-column:{i + 2}"> {key} </span>
+    <label class="checkbox" style="grid-column:{i + 2};grid-row:{2}">
+      <input type="checkbox" bind:checked={value.pindir} />
+      <span class="checkmark"></span>
+    </label>
+    <label class="checkbox" style="grid-column:{i + 2};grid-row:{3}">
+      <input
+        type="checkbox"
+        class="checkbox__input"
+        bind:checked={value.outmode}
+      />
+    </label>
   {/each}
 </div>
 <div class="out-help">
@@ -39,10 +34,29 @@
 
 <style>
   .analog-out {
-    display: flex;
-    flex-direction: row;
+    display: grid;
     justify-items: center;
     margin-bottom: 1.5em;
+    grid-template-rows: 3;
+    grid-gap: 1em;
+  }
+
+  input[type="checkbox"] {
+    width: 0;
+    height: 0;
+    visibility: hidden;
+  }
+
+  .checkbox {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    border: 1px solid #626262;
+    background: var(--bg-tertiary) no-repeat center;
+  }
+  .checkbox:has(input:checked) {
+    border-color: var(--main);
+    background-color: var(--main);
   }
 
   .out-help {
@@ -54,62 +68,5 @@
   .out-help p {
     line-height: 0.5; /* Adjust the line height as needed */
     margin-bottom: 0.5em; /* Adjust the margin as needed */
-  }
-
-  .row {
-    display: flex;
-    flex-direction: column;
-    margin-right: 1em;
-  }
-
-  .column {
-    display: flex;
-    flex-direction: column;
-    margin-right: 1em;
-  }
-
-  .header {
-    /* font-weight: bold; */
-    border-bottom: 2px solid #ccc;
-    padding-bottom: 0.5em;
-    margin-bottom: 0.5em;
-  }
-
-  .cell {
-    padding: 0.5em;
-    /* border-bottom: 1px solid #ccc; */
-  }
-
-  .cell:last-child {
-    border-bottom: none;
-  }
-
-  .checkbox {
-    display: flex;
-    align-items: center;
-    position: relative;
-    cursor: pointer;
-    user-select: none;
-  }
-
-  .checkbox input {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
-    height: 0;
-    width: 0;
-  }
-
-  .checkmark {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    border: 1px solid #626262;
-    background: var(--bg-tertiary);
-  }
-
-  .checkbox input:checked + .checkmark {
-    border-color: var(--main);
-    background-color: var(--main);
   }
 </style>
