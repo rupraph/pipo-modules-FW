15e59a536c176049ea54eaaf76a1fb1ac336dcff
Rup <rupertrenevier@hotmail.com>
2025-06-04 11:53:55 +0200

    mock demo

--- mock-server/src/ws.ts
+++ mock-server/src/ws.ts
@@ -17,7 +17,7 @@ function updateSens() {
   Object.keys(state.sensors).forEach((key) => {
     const min = s[key].min;
     const max = s[key].max;
-    state.sensors[key].value = min + ((max - min) * (sin + 1)) / 2;
+    state.sensors[key].value = 1.35;//min + ((max - min) * (sin + 1)) / 2;
   });
 }
 function getMsg(): string {
