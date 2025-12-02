b863574dbe75471b7286e1b8c594bd3a692d4ceb
Rup <rupertrenevier@hotmail.com>
2025-12-02 19:07:46 +0100

    completely disable hidio

--- src/main.cpp
+++ src/main.cpp
@@ -81,8 +81,9 @@ void setup() {  // by default on core 1
   String deviceName =
       "Pipo-" + String(config.general_config["PipoName"].as<String>());
   midiio.setup(deviceName.c_str());  //50k heap
+
 #ifndef DISABLE_USB_COMM
-  hidio.setup(config.general_config["HidMode"]);
+  // hidio.setup(config.general_config["HidMode"]);
 #endif
   delay(1000);
   print_reset_reason();
