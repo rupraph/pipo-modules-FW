bfebd7a2092a7bcfcf72d7c3b75e6165200481f0
Rup <rupertrenevier@hotmail.com>
2025-12-02 19:03:06 +0100

    do not send BT if not connected

--- src/midi/midiBLE.cpp
+++ src/midi/midiBLE.cpp
@@ -36,15 +36,21 @@ void OnDisconnected() {
 }
 
 void MidiBLEsendCC(int control, int value, int channel) {
-  MidiBle.sendControlChange(control, value, channel);
+  if (BTconnected) {
+    MidiBle.sendControlChange(control, value, channel);
+  }
 }
 
 void MidiBLEsendNoteOn(int note, int velocity, int channel) {
-  MidiBle.sendNoteOn(note, velocity, channel);
+  if (BTconnected) {
+    MidiBle.sendNoteOn(note, velocity, channel);
+  }
 }
 
 void MidiBLEsendNoteOff(int note, int velocity, int channel) {
-  MidiBle.sendNoteOff(note, velocity, channel);
+  if (BTconnected) {
+    MidiBle.sendNoteOff(note, velocity, channel);
+  }
 }
 
 #endif  //INCLUDE_BLE
