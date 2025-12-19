6172c53ec68f63c0886d2bbac2e8b430cd69ec2f
Rup <rupertrenevier@hotmail.com>
2025-12-19 12:31:59 +0100

    Revert "add USB serial to force detect name change"
    This reverts commit bdc30b03ce566f8689e4dfba84f11455bbb24570.

--- src/midi/midiUSB.cpp
+++ src/midi/midiUSB.cpp
@@ -1,5 +1,4 @@
 #include "midiUSB.h"
-#include <esp_mac.h>
 
 Adafruit_USBD_MIDI usb_midi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MidiUsb);
@@ -9,23 +8,6 @@ MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MidiUsb);
 void MidiUSBSetup(const char* deviceName) {
   log_i("MIDI USB setup: %s", deviceName);
 
-  // Create a unique serial number by combining MAC address + device name hash
-  // This forces macOS to recognize it as a different device when name changes
-  uint8_t mac[6];
-  esp_efuse_mac_get_default(mac);
-
-  // Simple hash of device name to make serial unique per name
-  uint32_t nameHash = 0;
-  for (const char* p = deviceName; *p; p++) {
-    nameHash = nameHash * 31 + *p;
-  }
-
-  // Serial: Last 3 MAC bytes (6 hex) + 16-bit hash (4 hex) = 10 chars
-  char serial[11];
-  snprintf(serial, sizeof(serial), "%02X%02X%02X%04X", mac[3], mac[4], mac[5],
-           (uint16_t)(nameHash & 0xFFFF));
-  serial[10] = '\0';  // Ensure null termination
-
   // Detach USB to change descriptors
   TinyUSBDevice.detach();
   delay(100);  // Wait for host to detect disconnect
@@ -33,10 +15,6 @@ void MidiUSBSetup(const char* deviceName) {
   // Set new descriptors while detached
   TinyUSBDevice.setManufacturerDescriptor("PipoInterfaces");
   TinyUSBDevice.setProductDescriptor(deviceName);
-
-  TinyUSBDevice.setSerialDescriptor(
-      serial);  // Different serial = different device to macOS
-
   // Re-attach USB - this triggers re-enumeration with new descriptors
   TinyUSBDevice.attach();
   delay(100);  // Wait for re-enumeration
@@ -44,7 +22,6 @@ void MidiUSBSetup(const char* deviceName) {
   // Now initialize MIDI
   MidiUsb.begin(MIDI_CHANNEL_OMNI);
 
-  log_i("MIDI USB setup complete - Serial: %s", serial);
   if (DEBUG_HEAP)
     pipoDebugHeap();
 }
